02dec2e67ec1883dd489e2cbeeaafa1929e5b643
Shriyash-Kadam <126370082+Shriyash-Kadam@users.noreply.github.com>
2023-05-02 10:44:17 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+difference = 0;
+rightWristX = 0;
+leftWristX = 0;
+
+function setup() {
+    video = createCapture(VIDEO);
+    video.size(550, 500);
+  
+    canvas = createCanvas(550, 550);
+    canvas.position(560,250);
+  
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+  }
+  
+  function modelLoaded() {
+    console.log('PoseNet Is Initialized!');
+  }
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+    }
+}
+
+
+function draw()
+{
+    background('#6495ed');
+}
+
